92d63c0761b8bf4ea51b5183180ca7721d0cf053
Ondrej Vasko <ondrej.vaskoo@gmail.com>
2018-12-16 23:37:47 +0100

    Small lint/refactor
    Signed-off-by: Ondrej Vasko <ondrej.vaskoo@gmail.com>

--- mdteach.sh
+++ mdteach.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Script options
 set -u
 
 # Colors
@@ -15,7 +16,7 @@ function ask_question() {
 
   clear
   if [ -n "$question" ] && [ -n "$answer" ]; then
-    printf "${CYAN}Question [Enter to cont.]: \n${NORMAL}%s?\n" "$question"
+    printf "${CYAN}Question [Enter to cont.]: \n${NORMAL}%s\n" "$question"
     read -r -s -n1 key
     [ "$key" = "q" ] && exit 0
 
@@ -27,48 +28,58 @@ function ask_question() {
   fi
 }
 
+
 function ask_input_question() {
   local question="$1"
   local answer="$2"
 }
 
+
 function randomize_questions() {
   :
 }
 
+
+# Look for markdown headers - lines starting with one or more `#`
 function parse_headers() {
+  local file="$1"
   local headers
-  headers=$(grep -E "^[#]+.*$" "$1")
+
+  headers=$(grep -E "^[#]+.*$" "$file")
   echo "$headers"
 }
 
+
+# Get line number where section starts
 function get_section_lineno() {
-  local section_name file line
-  section_name="$1"
-  file="$2"
+  local section_name="$1"
+  local file="$2"
+  local line
+
   line=$(grep -En "^$section_name$" "$file")
+
   # Strip line number from grep output
   echo "${line%%:*}"
 }
 
+
+# Reads section contents from start to the next lower level section
 function get_section_content() {
-  # Reads section contents from start to next lower level section
-  local file_content section_prefix section_lineno section_content
   local section_name="$1"
   local file="$2"
+  local file_content section_content section_lineno section_prefix
 
   section_prefix=${section_name%% *}
-
   section_lineno=$(get_section_lineno "$section_name" "$file")
   file_content=$(tail -n "+$section_lineno" "$file")
 
   # Read until we get into next section with same or lower level
   IFS=$'\n'; for line in $file_content; do
     if [[ "$line" =~ ^#{,${#section_prefix}}\ .*$ ]] && [[ "$line" != "$section_name" ]]; then
-      # Section |$line| is at same or lower level (topsection)
+      # Section is at same or lower level (section)
       break
     elif [[ "$line" =~ ^#{${#section_prefix},}\ .*$ ]]; then
-      # Section |$line| is at same or higher level (subsection)
+      # Section is at same or higher level (subsection)
       :
     fi
     section_content+="$line\n"
@@ -77,7 +88,8 @@ function get_section_content() {
   echo -e "$section_content"
 }
 
-function create_menu() {
+
+function show_menu() {
   local headers="$1"
   local selection
 
@@ -87,8 +99,9 @@ function create_menu() {
   echo "${selection//  /\#}"
 }
 
+
+# Find special words (delimited by **, __)
 function test_special_words() {
-  # Find special words (delimited by **, __)
   local content="$1"
   local word
 
@@ -100,6 +113,7 @@ function test_special_words() {
     fi
   done
 }
+
 
 function test_couples() {
   local content="$1"
@@ -122,14 +136,14 @@ function test_couples() {
   done
 }
 
+
+# Find line ending with `:`
+# Read next lines and look for list notations 1.|*|-|```
+# Skip empty lines
+# Skip code blocks
 function test_lists() {
-  # Find line ending with `:`
-  # Read next lines and look for list notations 1.|*|-|```
-  # Skip empty lines
-  # Skip code blocks
   local content="$1"
   local is_list=0
-  local is_code_block=0
   local question=""
   local answer=""
 
@@ -142,7 +156,6 @@ function test_lists() {
     if [[ "$line" =~ ^.*:$ ]]; then
       # Mark question
       is_list=1
-      is_code_block=0
 
       # Print last question and answer
       ask_question "$question" "$answer"
@@ -155,7 +168,6 @@ function test_lists() {
     if [[ "$line" =~ ^\`\`\`.* ]]; then
       # Mark code block
       is_list=0
-      is_code_block=1
       continue
     fi
     [ "$line" = "" ] && continue
@@ -165,6 +177,7 @@ function test_lists() {
   ask_question "$question" "$answer"
 }
 
+
 function main() {
   file="$1"
   if [ ! -f "$file" ]; then
@@ -173,23 +186,24 @@ function main() {
   fi
 
   headers=$(parse_headers "$file")
-  selection=$(create_menu "$headers")
-  content=$(get_section_content "$selection" "$file")
+  selected_section=$(show_menu "$headers")
+  section_content=$(get_section_content "$selected_section" "$file")
 
   selected_test=$(echo -e "Special words\nLists\nCouples" | dmenu -p "Select type of test:" -b -l 5)
   case $selected_test in
     "Special words" )
-      test_special_words "$content"
+      test_special_words "$section_content"
       ;;
     "Lists" )
-      test_lists "$content"
+      test_lists "$section_content"
       ;;
     "Couples" )
-      test_couples "$content"
+      test_couples "$section_content"
       ;;
     * )
       ;;
   esac
 }
+
 
 main "$@"
